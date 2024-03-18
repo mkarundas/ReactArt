b4515d0a9132da9ab63a5703cf9b607574bb69ff
Arundas MK <mkarundas@gmail.com>
2024-03-18 07:52:33 +0530

    adding missing style using tailwind css

--- react-art-tailwind/src/components/Input.jsx
+++ react-art-tailwind/src/components/Input.jsx
@@ -1,7 +1,18 @@
 
 export default function Input({label, invalid, ...props}) {
+
+  let labelClasses = "block mb-2 text-xs font-bold text-stone-200 uppercase";
+  let inputClasses = "w-full px-3 py-2 leading-tight border rounded shadow";
+  
+  if(invalid) {
+    labelClasses += " text-red-400";
+    inputClasses += " text-red-500 bg-red-100 border-red-300"
+  } else {
+    labelClasses += " text-stone-300";
+    inputClasses += " text-gray-700 bg-stone-300 "
+  }
     return <p>
-        <label className="">{label}</label>
-        <input {...props}/>
+        <label className={labelClasses}>{label}</label>
+        <input className={inputClasses} {...props}/>
     </p>
 }
